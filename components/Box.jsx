905f9ac5a70a3ff07cf2ee0407d27df98e2e2b17
dominggo1999 <arnolddominggo19@gmail.com>
2022-06-22 13:54:10 +0700

    test

---
+++ components/Box.jsx
@@ -0,0 +1,15 @@
+import tw from "tailwind-styled-components";
+import styled from "styled-components";
+
+
+export const Button = tw.button`
+  bg-red-500 
+  px-2 
+  py-1 
+  rounded-lg 
+  text-2xl 
+  font-semibold 
+  mt-20
+  ml-20 
+  inline-block
+`;
